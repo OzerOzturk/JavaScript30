0466e6ad3fcd2903bee2003ea374f417e7020a56
OzerOzturk <ozerozturk1636@gmail.com>
2021-05-24 22:18:48 +0300

    js and css clock project done.

--- 01-JavaScript-Drum-Kit/app.js
+++ 01-JavaScript-Drum-Kit/app.js
@@ -1,18 +1,23 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    
-    if (!audio) return; //it will stop the function from running
-    audio.currentTime = 0; //run back
-    audio.play();
-    key.classList.add("playing");
-  }
-  
-  function removeTransition(e) {
-    if (e.propertyName !== "transform" ) return; //if it's not a transform, skip it.
-    this.classList.remove("playing");
-  }
-  
-  const keys = document.querySelectorAll(".key");
-  keys.forEach((key) => key.addEventListener("transitioned", removeTransition));
-  window.addEventListener("keydown", (e) => playSound(e));
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+
+  console.log(e.propertyName);
+
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", (e) => playSound(e));
