dcded42f768162d2a28d3c56242741d76cc89207
OzerOzturk <ozerozturk1636@gmail.com>
2021-05-24 21:38:06 +0300

    fix for hang up problem of key

--- 01-JavaScript-Drum-Kit/app.js
+++ 01-JavaScript-Drum-Kit/app.js
@@ -1,17 +1,18 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key =document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
     if (!audio) return; //it will stop the function from running
     audio.currentTime = 0; //run back
     audio.play();
-    key.classList.add('playing');
-  };
-  
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform' ) return; //if it's not a transform, skip it.
-    this.classList.remove('playing');
+    key.classList.add("playing");
   }
   
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitioned', removeTransition));
-  window.addEventListener('keydown', playSound);
+  function removeTransition(e) {
+    if (e.propertyName !== "transform" ) return; //if it's not a transform, skip it.
+    this.classList.remove("playing");
+  }
+  
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) => key.addEventListener("transitioned", removeTransition));
+  window.addEventListener("keydown", (e) => playSound(e));
